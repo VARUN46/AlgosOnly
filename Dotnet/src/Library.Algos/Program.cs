5a01b41d9e475868667dd9e3bb05a14b7c6f2783
VARUN46 <vsetia46@gmail.com>
2021-09-09 21:16:50 +0530

    Arch Setup

---
+++ Dotnet/src/Library.Algos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library.Algos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Golang/src/go.mod
@@ -0,0 +1,3 @@
+module library/algos
+
+go 1.17
