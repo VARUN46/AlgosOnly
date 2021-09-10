6f8e270b1a62a9d632ea0d6b78a0ecc7c04a8545
VARUN46 <vsetia46@gmail.com>
2021-09-10 17:52:40 +0530

    Initial Commit

---
+++ Dotnet/src/Solution/Algos.Tests/Graphs/GraphTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Varun.Algos.NonLinear.Graphs;
+
+namespace Algos.Tests.Graphs
+{
+   [TestFixture]
+    public class GraphTests
+    {
+        [Test]
+        public void TestBasic()
+        {
+            AdjacencyList.Pair root = new()
+            {
+                Position = 1
+            };
+            AdjacencyList.Pair pair13 = new()
+            {
+                Position = 3
+            };
+            root.Next = pair13;
+            AdjacencyList.Pair pair35 = new()
+            {
+                Position = 5
+            };
+            pair13.Next = pair35;
+            AdjacencyList adjacencyList = new(1);
+            adjacencyList.AddPairs(root);
+            Assert.AreEqual(adjacencyList.DebugPrintFull(), "-->1-->3-->5|");
+        }
+    }
+}
---
+++ Dotnet/src/Solution/Varun.Algos/NonLinear/Graphs/AdjacencyList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varun.Algos.NonLinear.Graphs
+{
+    public class AdjacencyList
+    {
+        private Pair[] pairs;
+        private int index = -2;
+        public AdjacencyList(int length)
+        {
+            pairs = new Pair[length];
+            index = -1;
+        }
+
+        public void AddPairs(Pair pair)
+        {
+            pairs[++index] = pair;
+            Console.WriteLine(DebugPrintFull());
+        }
+
+        public string DebugPrintFull()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in pairs)
+            {
+                var currIndex = item;
+                while (currIndex != null)
+                {
+                    stringBuilder.Append("-->"+currIndex);
+                    currIndex = currIndex.Next;
+                }
+                stringBuilder.Append("|");
+                
+            }
+            return stringBuilder.ToString();
+        }
+
+        public class Pair
+        {
+            public int Position { get; set; }
+
+            public Pair Next { get; set; }
+
+            public override string ToString()
+            {
+                return Convert.ToString(Position);
+            }
+        }
+    }
+}
