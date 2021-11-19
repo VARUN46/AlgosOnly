3384d175ec85310cd3e4ddae70dfe4692c184948
VARUN46 <vsetia46@gmail.com>
2021-11-19 17:14:01 +0530

    DFS Algo

---
+++ Dotnet/src/Solution/Algos.Tests/Graphs/TraversalTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Varun.Algos.NonLinear.Graphs.Search;
+
+namespace Algos.Tests.Graphs
+{
+    [TestFixture]
+    public class TraversalTests
+    {
+        [Test]
+        public void DfsTest()
+        {
+            DFS.DirectedGraphAL graph = new DFS.DirectedGraphAL(4);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 3);
+            DFS dFS = new DFS(graph, 4);
+            dFS.Traverse(2);
+            var result = dFS.GetResult();
+            Assert.AreEqual(" 2 ->  0 ->  1 ->  3 -> ", result);
+        }
+    }
+}
---
+++ Dotnet/src/Solution/Varun.Algos/NonLinear/Graphs/Search/DFS.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varun.Algos.NonLinear.Graphs.Search
+{
+    public class DFS
+    {
+        private readonly bool[] isVisited;
+        private readonly DirectedGraphAL graph;
+        private readonly Stack<int> currentStack = new Stack<int>();
+        private StringBuilder stringBuilder = new StringBuilder();
+
+
+        public DFS(DirectedGraphAL directedGraph, int vertices)
+        {
+            isVisited = new bool[vertices + 1];
+            this.graph = directedGraph;
+
+        }
+
+        public void Traverse(int currentVertex)
+        {
+            CurrentContextLogic(currentVertex);
+            isVisited[currentVertex] = true;
+            var vertexNode = graph.GetAL()[currentVertex];
+            foreach (var vertexSelected in vertexNode)
+            {
+                if (!isVisited[vertexSelected])
+                {
+                    Traverse(vertexSelected);
+                }
+            }
+        }
+
+        private void CurrentContextLogic(int vertex)
+        {
+            stringBuilder.Append(" " + vertex + " -> ");
+        }
+
+        public string GetResult()
+        {
+            return stringBuilder.ToString();
+        }
+
+        public class DirectedGraphAL
+        {
+            private readonly List<LinkedList<int>> adjacencyList = new List<LinkedList<int>>();
+
+            public DirectedGraphAL(int vertices)
+            {
+                for (int i = 0; i < vertices; i++)
+                {
+                    adjacencyList.Add(new LinkedList<int>());
+                }
+            }
+
+            public void AddEdge(int source, int destination)
+            {
+                adjacencyList[source].AddLast(destination);
+            }
+
+            public List<LinkedList<int>> GetAL()
+            {
+                return adjacencyList;
+            }
+        }
+
+
+
+    }
+}
