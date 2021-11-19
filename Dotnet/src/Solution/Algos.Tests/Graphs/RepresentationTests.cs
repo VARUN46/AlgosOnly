709456fc9ded8766b282fcb34f4686f7c53ac779
VARUN46 <vsetia46@gmail.com>
2021-11-19 13:20:22 +0530

    Simple Adjacency List Added

---
+++ Dotnet/src/Solution/Algos.Tests/Graphs/RepresentationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Varun.Algos.NonLinear.Graphs.Structure;
+
+namespace Algos.Tests.Graphs
+{
+    [TestFixture]
+    internal class RepresentationTests
+    {
+        [Test]
+        public void CreateAdjacencyList()
+        {
+            GraphAL graph = new GraphAL(5);
+            graph.AddEdgeUndirected(0, 1);
+            graph.AddEdgeUndirected(0, 4);
+            graph.AddEdgeUndirected(1, 2);
+            graph.AddEdgeUndirected(1, 3);
+            graph.AddEdgeUndirected(1, 4);
+            graph.AddEdgeUndirected(2, 3);
+            graph.AddEdgeUndirected(3, 4);
+            var resultData = graph.GetLinkedList();
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(5, resultData.Count); //Has 0,1,2,3,4
+                Assert.AreEqual(1, resultData[0].First.Value);
+                Assert.AreEqual(4, resultData[0].Last.Value);
+                Assert.AreEqual(4, resultData[0].Last.Value);    
+            });
+
+        }
+    }
+}
---
+++ Dotnet/src/Solution/Varun.Algos/NonLinear/Graphs/Structure/GraphAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varun.Algos.NonLinear.Graphs.Structure
+{
+    public class GraphAL
+    {
+        private List<LinkedList<int>> adjacencyListData;
+
+        /// <summary>
+        /// //Empty Structure Is Initialized
+        /// </summary>
+        /// <param name="vertices">Number of Vertex</param>
+        public GraphAL(int vertices)
+        {  
+            adjacencyListData = new List<LinkedList<int>>();
+            for (int i = 0; i < vertices; i++)
+            {
+                adjacencyListData.Add(new LinkedList<int>());
+            }
+        }
+
+        public void AddEdgeUndirected(int source,int destination)
+        {
+            adjacencyListData[source].AddLast(destination);
+            adjacencyListData[destination].AddLast(source);
+        }
+
+        public List<LinkedList<int>> GetLinkedList()
+        {
+            return adjacencyListData;
+        }
+    }
+}
