60aea5004e96f98d57e734a78fafc469136fe888
VARUN46 <vsetia46@gmail.com>
2021-11-19 14:03:21 +0530

    Matrix DS Representation

--- Dotnet/src/Solution/Algos.Tests/Graphs/RepresentationTests.cs
+++ Dotnet/src/Solution/Algos.Tests/Graphs/RepresentationTests.cs
@@ -30,6 +30,39 @@ namespace Algos.Tests.Graphs
                 Assert.AreEqual(4, resultData[0].Last.Value);
                 Assert.AreEqual(4, resultData[0].Last.Value);    
             });
+        }
+
+        [Test]
+        public void CreateMatrix()
+        {
+            GraphM graph = new GraphM(5);
+            graph.AddEdgeUndirected(0, 1);
+            graph.AddEdgeUndirected(0, 4);
+            graph.AddEdgeUndirected(1, 2);
+            graph.AddEdgeUndirected(1, 3);
+            graph.AddEdgeUndirected(1, 4);
+            graph.AddEdgeUndirected(2, 3);
+            graph.AddEdgeUndirected(3, 4);
+            var resultData = graph.GetMatrix();
+            Assert.Multiple(() => {
+                Assert.AreEqual(5, resultData.Count);
+                Assert.IsTrue(resultData.All(c=>c.Count==5));
+                Assert.AreEqual(1,resultData[0][1]);
+                Assert.AreEqual(1, resultData[0][4]);
+                Assert.AreEqual(1, resultData[1][2]);
+                Assert.AreEqual(1, resultData[1][3]);
+                Assert.AreEqual(1, resultData[1][4]);
+                Assert.AreEqual(1, resultData[2][3]);
+                Assert.AreEqual(1, resultData[3][4]);
+               
+                Assert.AreEqual(1, resultData[1][0]);
+                Assert.AreEqual(1, resultData[4][0]);
+                Assert.AreEqual(1, resultData[2][1]);
+                Assert.AreEqual(1, resultData[3][1]);
+                Assert.AreEqual(1, resultData[4][1]);
+                Assert.AreEqual(1, resultData[3][2]);
+                Assert.AreEqual(1, resultData[4][3]);
+            });
 
         }
     }
---
+++ Dotnet/src/Solution/Varun.Algos/NonLinear/Graphs/Structure/GraphM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varun.Algos.NonLinear.Graphs.Structure
+{
+    public class GraphM
+    {
+        private readonly List<List<int>> matrix = new List<List<int>>();
+        public GraphM(int vertices)
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                matrix.Add(Enumerable.Repeat(0, vertices).ToList());
+                
+            }
+        }
+
+        public void AddEdgeUndirected(int source,int destination)
+        {
+            matrix[source][destination] = 1;
+            matrix[destination][source] = 1;
+        }
+
+        public List<List<int>> GetMatrix()
+        {
+            return matrix;
+        }
+
+    }
+}
