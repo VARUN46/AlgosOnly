8099beaf82b3f7f68a46559c2edfc6ee3822713e
VARUN46 <vsetia46@gmail.com>
2021-11-21 21:01:17 +0530

    Simple Pattern Matching

---
+++ Dotnet/src/Solution/Algos.Tests/Strings/PatternTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Varun.Algos.Strings;
+
+namespace Algos.Tests.Strings
+{
+    [TestFixture]
+    public class PatternTests
+    {
+        [Test]
+        [TestCase("AABA", new[] { 0,9,13 })]
+        public void DoSimplePatternMatching(string pattern,int[] actual)
+        {
+            SimplePatternMatching matching = new SimplePatternMatching("AABAACAADAABAAABAA");
+            var expected = matching.Match(pattern);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ Dotnet/src/Solution/Varun.Algos/Strings/SimplePatternMatching.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varun.Algos.Strings
+{
+    public class SimplePatternMatching
+    {
+        private readonly string @string;
+        public SimplePatternMatching(string @string)
+        {
+            this.@string = @string;
+        }
+
+        public int[] Match(string pattern)
+        {
+            List<int> matchIndexes = new List<int>();
+            int stringLength = @string.Length;
+            int patternLength = pattern.Length;
+           
+            for (int stringIndex = 0; stringIndex <= stringLength - patternLength; stringIndex++)
+            {
+                int patternIndex;
+                for (patternIndex = 0; patternIndex < patternLength; patternIndex++)
+                {
+                    if (@string[stringIndex + patternIndex] != pattern[patternIndex])
+                    {
+                        break;
+                    }
+                   
+                }
+                if (patternIndex == patternLength)
+                {
+                    matchIndexes.Add(stringIndex);
+                }
+            }
+
+
+            return matchIndexes.ToArray();
+        }
+    }
+}
